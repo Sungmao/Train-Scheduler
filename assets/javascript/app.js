516a35484d9a88614ba2f495e0bbce13480387eb
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-09-12 23:54:17 -0500

    initial html and javascript with submit working

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAKMvvgPsMvDjUruq0B2tD1fm9AmPgEHkU",
+    authDomain: "train-scheduler-24958.firebaseapp.com",
+    databaseURL: "https://train-scheduler-24958.firebaseio.com",
+    storageBucket: "train-scheduler-24958.appspot.com",
+};
+  
+firebase.initializeApp(config);
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
+
+
+$("#addInfor").on("click", function(){
+
+	var trainName = $('#trainNameInput').val().trim();
+	var destination = $('#destinationInput').val().trim();
+	var firstTrainTime = $('#firstTrainTimeInput').val().trim();
+	var frequency = $('#frequencyInput').val().trim();
+
+	var trainNameTd = $('<td>').text(trainName);
+	var destinationTd = $('<td>').html(destination);
+	var firstTrainTimeTd = $('<td>').html(firstTrainTime);
+	var frequencyTd = $('<td>').html(frequency);
+
+	var tableRow = $('<tr>').append(trainNameTd, destinationTd, firstTrainTimeTd, frequencyTd);
+	$('#trainInforView').append(tableRow);
+
+	//database.ref().set({
+	//	trainName: trainName,
+	//	destination: destination,
+	//	firstTrainTime: firstTrainTime,
+	//	frequency: frequency
+	//});
+
+	return false;
+
+
+});
